5d5db9665ccc485a5c260c682985b545905f06af
Sagar2004 <71272983+Sagar2004@users.noreply.github.com>
2020-09-15 16:36:58 +0530

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,14 @@
+# APNI DICTIONARY.
+
+d4 = {'Sagar':'15.12.2004',
+      'Rohit':'25.1.2005',
+      'Ankit':'26.2.2004',
+      'Swaraj':'10.1.2005',
+      'Aai':'6.5.1981',
+      'Baba':'5.9.1974',
+      'Sampada':'18.10.2010',
+      'Manas':'13.3.2004',
+      'Shubhankar':'26.2.2005',}
+name = input("Enter a name and know his birthdate : ")
+print(d4[name])
+
